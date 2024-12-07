4f233cb57c1baffc2ca7d5510defbf53c3170b7d
nashehannafii <nashehannafii@gmail.com>
2024-12-07 21:10:58 +0700

    [Nasheh] testing

--- all/app.js
+++ all/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const appConfig = require("./config/config");
+const appConfig = require("../config/config");
 const setupWebSocketServer = require("./routes/webSocket");
 const routes = require("./routes/route");
 
