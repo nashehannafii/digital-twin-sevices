650fa3942506d44ce023b3fda595420fbb3a5975
nashehannafii <nashehannafii@gmail.com>
2024-12-07 21:08:12 +0700

    [Nasheh] update lagi

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express(); // Buat instance aplikasi Express
 
 // Endpoint "/" untuk menampilkan "Hello, World!"
 app.get('/', (req, res) => {
-  res.json({ message: "Hello, World!" });
+  res.json({ message: "Hello, Worldd!" });
 });
 
 // Menjalankan server di port 3000
